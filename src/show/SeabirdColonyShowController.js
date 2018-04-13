799be25d841eccc4ee448aba93a9bc4f040b1b61
srldl <siri.uldal@gmail.com>
2018-04-13 10:59:45 +0200

    show works

--- src/show/SeabirdColonyShowController.js
+++ src/show/SeabirdColonyShowController.js
@@ -1,7 +1,9 @@
 'use strict';
 
 var L = require('leaflet');
-L.Icon.Default.imagePath = 'node_modules/dist/images/';
+L.Icon.Default.imagePath = "../assets/images/";
+
+
 
 var SeabirdColonyShowController = function($controller, $routeParams,
   $scope, $q, SeabirdColony, npdcAppConfig) {
@@ -13,74 +15,37 @@ var SeabirdColonyShowController = function($controller, $routeParams,
   });
   $scope.resource = SeabirdColony;
 
-  var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-       maxZoom: 18,
-       attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ'
-     }),
-     latlng = L.latLng(78.000, 16.000);
-
-  var map = L.map('mapid', {center: latlng, zoom: 4, layers: [tiles]});
-
-
-  setTimeout(function () {
-    map.invalidateSize();
-  }, 0);
-
 
   let show = function() {
 
     $scope.show().$promise.then((seabirdColony) => {
+      var coord = (78.223333, 15.646944);
       if ((seabirdColony.geometry)&&(seabirdColony.geometry.geometries)) {
-         console.log("test", JSON.stringify(seabirdColony.geometry.geometries[1]));
+      //   console.log("test", seabirdColony.geometry.geometries[1].coordinates[0][0]);
+
+      //Need to reverse coord because geojson use opposite listing
+       coord = (seabirdColony.geometry.geometries[1].coordinates[0][0]);
       }
 
-      var places = [{
-                 "type": "Feature",
-                 "geometry": {
-                 "type": "Point",
-                 "coordinates": [16.000, 78.000]
-               },
-                 "properties": {
-                 "name": "Svalbard"
-              }
-              },
-              {
-                "type": "Feature",
-                "geometry": {
-                "type": "LineString",
-                "coordinates": [
-                      [16.000, 78.000], [18.56, 79.89], [17.56, 78.67], [18.78, 79.45]
-                      ]
-                },
-                "properties": {
-                "name": "Svalbard"
-              }
-              },
-              {
-                "type": "Feature",
-                "geometry": {
-                "type": "Polygon",
-                "coordinates": [ [
-                      [20.000, 88.000], [18.56, 79.89], [17.56, 78.67], [18.78, 79.45], [20.000, 88.000]
-                ]  ]
-              },
-              "properties": {
-                "id": "old"
-              }
-          } ];
 
+
+
+        var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+             maxZoom: 18,
+             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ'
+           }),
+
+           latlng = L.latLng(coord[1],coord[0]);
+
+        var map = L.map('mapid', {center: latlng, zoom: 9, layers: [tiles]});
 
       L.geoJSON(seabirdColony.geometry).addTo(map);
 
 
-/*      var polygon = L.polygon([
-        [78.000, 16.000],
-        [78.000, 17.000],
-        [79.000, 17.000]
-    ]).addTo(map); */
+    //    setTimeout(function () {
+  //        map.invalidateSize();
+  //      }, 0);
 
-
-      //$scope.mapOptions.geojson = "geojson";
     });
 
   };
