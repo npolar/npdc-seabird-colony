1bcbafbdcd2f222df0deec09ed473607daa691bf
srldl <siri.uldal@gmail.com>
2018-04-16 08:33:23 +0200

    works

--- src/show/SeabirdColonyShowController.js
+++ src/show/SeabirdColonyShowController.js
@@ -1,6 +1,7 @@
 'use strict';
 
-var L = require('leaflet');
+//var L = require('../../node_modules/leaflet');
+require('npdc-common/src/wrappers/leaflet');
 L.Icon.Default.imagePath = "../assets/images/";
 
 
@@ -16,35 +17,38 @@ var SeabirdColonyShowController = function($controller, $routeParams,
   $scope.resource = SeabirdColony;
 
 
+
+
   let show = function() {
 
     $scope.show().$promise.then((seabirdColony) => {
-      var coord = (78.223333, 15.646944);
+
+       var coord = (78.223333, 15.646944);
       if ((seabirdColony.geometry)&&(seabirdColony.geometry.geometries)) {
       //   console.log("test", seabirdColony.geometry.geometries[1].coordinates[0][0]);
 
       //Need to reverse coord because geojson use opposite listing
        coord = (seabirdColony.geometry.geometries[1].coordinates[0][0]);
+
       }
 
+      var tiles = L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}/', {
+           maxZoom: 18,
+           attribution: 'Esmapri, DigitalGlobe, GeoEye, Earthstar Geographics, CNES/Airbus DS, USDA, USGS, AEX, Getmapping, Aerogrid, IGN, IGP, swisstopo, and the GIS User Community'
+         }),
+
+         latlng = L.latLng(coord[1],coord[0]);
+
+      var map = L.map('mapid2', {center: latlng, zoom: 9, layers: [tiles]});
 
 
-
-        var tiles = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-             maxZoom: 18,
-             attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Points &copy 2012 LINZ'
-           }),
-
-           latlng = L.latLng(coord[1],coord[0]);
-
-        var map = L.map('mapid', {center: latlng, zoom: 9, layers: [tiles]});
 
       L.geoJSON(seabirdColony.geometry).addTo(map);
 
 
-    //    setTimeout(function () {
-  //        map.invalidateSize();
-  //      }, 0);
+        setTimeout(function () {
+          map.invalidateSize();
+        }, 0);
 
     });
 
