19e26fb7ee8703b0a1e2a79af0e491da17095ea3
srldl <siri.uldal@gmail.com>
2018-04-24 14:34:19 +0200

    work except geoJson obj

--- src/show/leafletMap.js
+++ src/show/leafletMap.js
@@ -3,45 +3,53 @@
 var L = require('../../node_modules/leaflet');
 L.Icon.Default.imagePath = "../assets/images/";
 
+
 let leafletMap = function () {
 "ngInject";
 
+
 return   {
-    restrict: 'EA',
+    restrict: 'A',
     scope: {
             polygon: '@'
     },
     template: '<div local-map style="height: 300px;"></div>',
-    link: function ($scope, element, attrs) {
-       console.log("scope",$scope);
-       console.log("element", element);
-       console.log("attrs", attrs);
-        var coord = [15.646944,78.223333];
+  controller:  ['$scope', '$element', '$attrs', function($scope, $element, $attrs) {
 
-        if ((attrs.polygon)&&(attrs.polygon.geometries)) {
-        //Need to reverse coord because geojson use opposite listing
-         coord = attrs.polygon.geometries[1].coordinates[0][0];
-         console.log("coord", coord);
+      var coord = [15.646944,78.223333]; //std coord
 
-        }
+      var map = L.map('map2').setView([coord[1],coord[0]], 9);
 
-        var map = L.map('map2').setView([coord[1],coord[0]], 9);
+      L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}/', {
+        attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+        maxZoom: 18,
+      }).addTo(map);
 
-        L.tileLayer('https://services.arcgisonline.com/ArcGIS/rest/services/World_Imagery/MapServer/tile/{z}/{y}/{x}/', {
-    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
-    maxZoom: 18,
-}).addTo(map);
+       setTimeout(function () {
+         map.invalidateSize();
+       }); //setTimeout
+
+      $scope.$watch('polygon', function() {
+             console.log("scopecontr", $scope.polygon);
+             var places = $scope.polygon;
 
 
-         setTimeout(function () {
-           map.invalidateSize();
-         }, 0);
+            //  console.log("scopecontr2", $scope.polygon['geometries']);
+            if (places) {
+                 console.log("places", places);
+                L.geoJSON(places).addTo(map);
 
-    L.geoJSON(attrs.polygon).addTo(map);
-   L.marker([coord[1],coord[0]]).addTo(map);
+            }
 
-     }
-    };
+             //Need to reverse coord because geojson use opposite listing
+            //  var  coord = $scope.polygon[0].geometries[1].coordinates[0][0];
+            //  console.log("coord", coord);
+            //   L.geoJSON($scope.polygon).addTo(map);
+            //  L.marker([coord[1],coord[0]]).addTo(map);
+
+      });
+  }]
+};
 };
 
 module.exports = leafletMap;
