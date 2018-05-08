110f4490b4b29c4702cc601c0e10f874cebe02c3
srldl <siri.uldal@gmail.com>
2018-05-08 15:39:57 +0200

    fullscreen added

--- README.md
+++
@@ -1,2 +0,0 @@
-# npdc-seabird-colony
-Show information about the seabird colonies at Svalbard.
--- gulpfile.js
+++
@@ -1,5 +0,0 @@
-var gulp = require('gulp');
-var npdcGulp = require('npdc-gulp');
-var config = npdcGulp.baseConfig;
-config.COMMON_VERSION = '4.11.2';
-npdcGulp.loadAppTasks(gulp, config);
