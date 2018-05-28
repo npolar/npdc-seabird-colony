608318646f7eed8072776da1eca17229a124a064
srldl <siri.uldal@gmail.com>
2018-05-28 14:45:22 +0200

    search priority fixed

--- src/search/SeabirdColonySearchController.js
+++ src/search/SeabirdColonySearchController.js
@@ -8,9 +8,11 @@ var SeabirdColonySearchController = function ($scope, $location, $controller, $f
   $scope.resource = SeabirdColony;
 
  npdcAppConfig.search.local.results.detail = (entry) => {
-     let updatedText = NpolarTranslate.translate('updated');
-     let r = (entry.colony_name).charAt(0).toUpperCase() +  (entry.colony_name).slice(1) + ", "+ updatedText +":";
-     return r+` ${(entry.updated.split('T')[0])}`;
+     let updatedText = NpolarTranslate.translate('start date');
+     //let r = (entry.colony_name).charAt(0).toUpperCase() +  (entry.colony_name).slice(1) + ", "+ updatedText +":";
+     let r =  updatedText +":";
+     return r+` ${(entry.start_date.split('T')[0])}`;
+
  };
 
   npdcAppConfig.cardTitle = "Seabird Colony Archive";
@@ -21,9 +23,9 @@ var SeabirdColonySearchController = function ($scope, $location, $controller, $f
   let query = function() {
     let defaults = {
       limit: "50",
-      sort: "-updated",
-      fields: 'species,id,updated,zone,colony_name',
-      facets: 'species,zone'};
+      sort: "colony_name,species,-start_date",
+      fields: 'species,id,updated,zone,colony_name,start_date',
+      facets: 'colony_name,species,start_date'};
 
     let invariants = $scope.security.isAuthenticated() ? {} : {} ;
     return Object.assign({}, defaults, invariants);
